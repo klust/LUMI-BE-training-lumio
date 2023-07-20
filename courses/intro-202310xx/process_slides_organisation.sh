f8e24918cd00dfd344f9715d94331211f81e7367
Kurt Lust <Kurt.Lust@uantwerpen.be>
2023-07-20 18:46:51 +0200

    Process slides for the LUMI organisation slide deck, and do the restructuring.

---
+++ courses/intro-202310xx/process_slides_organisation.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/env bash
+
+# Some variables.
+slidestack="LUMI-BE-Intro-202310XX-03-organisation"
+
+slidealias=( \
+    "Title" \
+    "WhoPays" \
+    "ProjectsUsers_1" \
+    "ProjectsUsers_2" \
+    "ProjectManagement" \
+    "FileSpaces_1" \
+    "FileSpaces_2" \
+    "Access" \
+    "DataTransfer" \
+)
+
+#
+# Automatically set variables
+#
+training="${PWD##*/LUMI-BE-training-lumio/courses/}"
+if [[ "$training" == "$PWD" ]]
+then
+    echo "Failed to find the name of the course."
+    exit
+fi
+
+#
+# Do the actual work of renaming the slides
+#
+
+cd "../../public/$training/img/$slidestack"
+
+number=0
+for name in ${slidealias[@]}
+do 
+    number=$((number+1))
+    echo "Moving Dia${number}.png to ${name}.png"
+    /bin/mv -f "Dia${number}.png" "${name}.png"
+done
+
