8f5dc3105b19ae54d1b1cae95e1a21aa5e5010a3
Kurt Lust <Kurt.Lust@uantwerpen.be>
2023-07-24 16:19:24 +0200

    Extension of the script to process the slides of the modules to also process saved images, and a script to delete a course bucket.

--- courses/intro-202310xx/process_slides_modules.sh
+++ courses/intro-202310xx/process_slides_modules.sh
@@ -47,6 +47,53 @@ slidealias=( \
 )
 
 #
+# Not really needed, we'll just check if an ImgXX file exists before
+# doing the move.
+#
+imgalias=( \
+    "None" \
+    "None" \
+    "None" \
+    "None" \
+    "BenefitsHierarchyDemo" \
+    "None" \
+    "None" \
+    "None" \
+    "ModuleSpiderCommand_1" \
+    "ModuleSpiderCommand_2" \
+    "ModuleSpiderCommand_3" \
+    "ModuleSpiderFFTW" \
+    "ModuleSpiderFFTWVersion_1" \
+    "ModuleSpiderFFTWVersion_2" \
+    "None" \
+    "ModuleSpiderGnuplot_1" \
+    "ModuleSpiderGnuplot_2" \
+    "ModuleSpiderGnuplotVersion_1" \
+    "ModuleSpiderGnuplotVersion_2" \
+    "None" \
+    "ModuleSpiderCMake" \
+    "ModuleSpiderCMakeVersion_1" \
+    "ModuleSpiderCMakeVersion_2" \
+    "None" \
+    "ModuleKeywordHTTPS_1" \
+    "ModuleKeywordHTTPS_2" \
+    "ModuleKeywordHTTPS_3" \
+    "ModuleKeywordHTTPS_4" \
+    "ModuleKeywordHTTPS_5" \
+    "None" \
+    "ModuleAvail_1" \
+    "ModuleAvail_2" \
+    "ModuleAvail_3" \
+    "ModuleAvail_4" \
+    "ModuleAvail_5" \
+    "ModuleAvail_6" \
+    "ChangingDisplayStyle" \
+    "GettingHelp" \
+    "NoteCaching" \
+    "NoteOtherCommands" \
+)
+
+#
 # Automatically set variables
 #
 training="${PWD##*/LUMI-BE-training-lumio/courses/}"
@@ -66,7 +113,27 @@ number=0
 for name in ${slidealias[@]}
 do 
     number=$((number+1))
-    echo "Moving Dia${number}.png to ${name}.png"
-    /bin/mv -f "Dia${number}.png" "${name}.png"
+    if [ -f "Dia$number.png" ]
+    then
+        echo "Moving Dia${number}.png to ${name}.png"
+        /bin/mv -f "Dia${number}.png" "${name}.png"
+    fi
+done
+
+#
+# Do the actual work of renaming the images.
+#
+
+cd "../${slidestack}-img"
+
+number=0
+for name in ${slidealias[@]}
+do 
+    number=$((number+1))
+    if [ -f "img$number.png" ]
+    then
+        echo "Moving img${number}.png to img-${name}.png"
+        /bin/mv -f "img${number}.png" "img-${name}.png"
+    fi
 done
 
