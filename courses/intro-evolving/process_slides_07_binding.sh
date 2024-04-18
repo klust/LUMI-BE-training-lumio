30fba8e14dc8bd7fa300ce6bc9c121176e59da5a
Kurt Lust <Kurt.Lust@uantwerpen.be>
2024-04-18 20:53:10 +0200

    Extra slide for the binding chapter.

--- courses/intro-evolving/process_slides_07_binding.sh
+++ courses/intro-evolving/process_slides_07_binding.sh
@@ -31,6 +31,7 @@ slidealias=( \
     "ROCRMechanismLinearGCD2" \
     "ROCRMechanismLinearCCD1" \
     "ROCRMechanismLinearCCD2" \
+    "ROCRMechanismLinearCCD7" \
     "ROCRMechanismGreenRing1" \
     "ROCRMechanismGreenRing2" \
     "ROCRMechanismGreenRing3" \
