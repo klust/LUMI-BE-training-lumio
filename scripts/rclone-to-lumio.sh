76585acb18e65f0b1b31bc3033f2fbaaedf7a71d
Kurt Lust <Kurt.Lust@uantwerpen.be>
2024-05-14 11:41:05 +0200

    Trying to remove DS_Store files.

--- scripts/rclone-to-lumio.sh
+++ scripts/rclone-to-lumio.sh
@@ -20,6 +20,7 @@ repo="lumi-${projectid}-public"
 if [ -d $directory ]
 then
     echo "Found public data for training '$training', pushing now."
+    find $directory -name ".DS_store" -exec /bin/rm '{}' \;
     rclone mkdir "$repo:$bucket/$training"
     rclone copy --bwlimit ${RCLONE_BWLIMIT:-0} "$directory" "$repo:$bucket/$training"
 else
@@ -34,6 +35,7 @@ repo="lumi-${projectid}-private"
 if [ -d $directory ]
 then
     echo "Found private data for training '$training', pushing now."
+    find $directory -name ".DS_store" -exec /bin/rm '{}' \;
     rclone mkdir "$repo:$bucket/$training"
     rclone copy --bwlimit ${RCLONE_BWLIMIT:-0} "$directory" "$repo:$bucket/$training"
 else
