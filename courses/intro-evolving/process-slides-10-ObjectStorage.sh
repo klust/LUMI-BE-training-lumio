a33774402feb776984af04ce7936b8cc0012ac6c
Kurt Lust <Kurt.Lust@uantwerpen.be>
2025-02-11 16:46:18 +0100

    Correcting a typo.

--- courses/intro-evolving/process-slides-10-ObjectStorage.sh
+++ courses/intro-evolving/process-slides-10-ObjectStorage.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # Some variables.
-slidestack="LUMI-BE-intro-evolving-10-ObjectStorage"
+slidestack="LUMI-BE-Intro-evolving-10-ObjectStorage"
 
 slidealias=( \
     "Title" \
